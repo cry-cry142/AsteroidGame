3668c23f34f18089c8774ef78660a60a55c6a30f
Кирилл Касаткин <cry-cry142@yandex.ru>
2020-03-01 22:06:18 +0300

    обновление

---
+++ Tests/TestConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Gamer gamer = new Gamer();
+            gamer.Name = "Игрок 1";
+            gamer.DayOfBirth = new DateTime(1983, 12, 4, 21, 2, 14);
+        }
+    }
+    class Gamer
+    {
+        public string Name;
+        public DateTime DayOfBirth;
+    }
+}
